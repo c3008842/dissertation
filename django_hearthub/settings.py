0504ff40d0da92590bf2211dd6cc561792fe2be4
c3008842 <c3008842@hallam.shu.ac.uk>
2024-10-01 13:46:48 +0100

    made some changes on settings.py and some html pages

--- django_hearthub/settings.py
+++ django_hearthub/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from pathlib import Path
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +59,7 @@ ROOT_URLCONF = 'django_hearthub.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,8 +80,12 @@ WSGI_APPLICATION = 'django_hearthub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '',  # Database name from phpMyAdmin
+        'USER': 'root',       # MySQL username (root by default)
+        'PASSWORD': '',   # MySQL password (empty by default)
+        'HOST': 'localhost',           # Database host (localhost for local development)
+        'PORT': '3306',                # MySQL port (3306 by default)
     }
 }
 
--- remote_monitoring/views.py
+++ remote_monitoring/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello, world!")
+    return render(request, 'index.html')
